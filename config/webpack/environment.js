c75d969e5c91603bf0038c4e32934b046210b5e5
森恭平 <morikyohei@morikyouheinoMacBook-Air.local>
2021-06-02 01:04:39 +0900

    Add: webpackrのインストールとデータベース作成

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,5 +1,7 @@
 const { environment } = require('@rails/webpacker')
+
 const webpack = require('webpack')
+
 environment.plugins.prepend('Provide',
   new webpack.ProvidePlugin({
     $: 'jquery/src/jquery',
@@ -7,4 +9,5 @@ environment.plugins.prepend('Provide',
     Popper: ['popper.js', 'default']
   })
 )
+
 module.exports = environment
