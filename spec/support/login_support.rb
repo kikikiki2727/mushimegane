3a83a011063fa96641acf2917ab6442299303b49
森恭平 <morikyohei@morikyouheinoMacBook-Air.local>
2021-06-05 01:31:05 +0900

    Add: ログインログアウトのシステムスペックを追加

--- spec/support/login_support.rb
+++ spec/support/login_support.rb
@@ -1,7 +1,7 @@
 module LoginSupport
   def login(user)
     visit login_path
-    fill_in 'email', with: 'user.email'
+    fill_in 'email', with: user.email
     fill_in 'password', with: 'password'
     click_on 'ログイン'
   end
