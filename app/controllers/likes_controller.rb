7c8070aadabeee513255f2c8bfe4401fcffddbb4
森恭平 <morikyohei@morikyouheinoMacBook-Air.local>
2021-06-27 16:44:57 +0900

    Fix: コメントへのいいね機能をajax化

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,14 +1,12 @@
 class LikesController < ApplicationController
 
   def create
-    comment = Comment.find(params[:comment_id])
-    current_user.like_comment(comment)
-    redirect_back fallback_location: root_path
+    @comment = Comment.find(params[:comment_id])
+    current_user.like_comment(@comment)
   end
 
   def destroy
-    comment = Like.find(params[:id]).comment
-    current_user.unlike_comment(comment)
-    redirect_back fallback_location: root_path
+    @comment = Like.find(params[:id]).comment
+    current_user.unlike_comment(@comment)
   end
 end
