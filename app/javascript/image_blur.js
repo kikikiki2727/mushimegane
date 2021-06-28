7109d96b0eae39497a988badf9e93073d0c018ee
森恭平 <morikyohei@morikyouheinoMacBook-Air.local>
2021-06-28 10:49:23 +0900

    Fix: 虫一覧ページの画像にモザイクを付け外しできるように修正

--- app/javascript/image_blur.js
+++ app/javascript/image_blur.js
@@ -1,17 +1,19 @@
 document.addEventListener("turbolinks:load", function() {
   $(function() {
     $('.blur_button').click(function(){
-      $('.blur').removeClass('blur_active');
-      $('.blur_button').addClass('hide_button');
-      $('.hide_blur_button').removeClass('hide_button');
+      let blur_button_id = $(this).val();
+      $(`.blur_${blur_button_id}`).removeClass('blur_active');
+      $(`.blur_button_${blur_button_id}`).addClass('hide_button');
+      $(`.hide_blur_button_${blur_button_id}`).removeClass('hide_button');
     })
   });
 
   $(function() {
     $('.hide_blur_button').click(function(){
-      $('.blur').addClass('blur_active');
-      $('.hide_blur_button').addClass('hide_button');
-      $('.blur_button').removeClass('hide_button');
+      let hide_blur_button_id = $(this).val();
+      $(`.blur_${hide_blur_button_id}`).addClass('blur_active');
+      $(`.hide_blur_button_${hide_blur_button_id}`).addClass('hide_button');
+      $(`.blur_button_${hide_blur_button_id}`).removeClass('hide_button');
     })
   })
 })
