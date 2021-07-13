61c32279090b0838b86f5d55056e162fb344fd7c
森恭平 <morikyohei@morikyouheinoMacBook-Air.local>
2021-07-14 02:01:34 +0900

    Fix: 画像プレビューのコードを修正

--- app/javascript/image_search_preview.js
+++ app/javascript/image_search_preview.js
@@ -1,17 +1,14 @@
 document.addEventListener("turbolinks:load", function() {
   $(function(){
-    $("#input_image").change(function(e){
-      var file = e.target.files[0];
+    $("#input_image").change(function(){
+      var file = $(this).prop('files')[0];
       var reader = new FileReader
-      console.log(file)
 
       reader.onload = (function(){
-        return function(e){
-          $("#image_preview").css("display", "");
-          $("#image_preview").css({"width":"300px", "height":"300px"});
-          $("#image_preview").attr("src", e.target.result);
-        };
-      })(file);
+        $("#image_preview").css("display", "");
+        $("#image_preview").css({"width":"300px", "height":"300px"});
+        $("#image_preview").attr("src", reader.result);
+      });
       reader.readAsDataURL(file);
     })
   })
