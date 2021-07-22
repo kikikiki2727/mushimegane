a717b01c50addad3a84985b1af6d7b358c3070ee
森恭平 <morikyohei@morikyouheinoMacBook-Air.local>
2021-07-22 23:02:41 +0900

    Fix: rubocopのdependetオプション必須の設定を変更

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,6 +2,6 @@ module ApplicationHelper
   def pages_title(page_title = '')
     base_title = 'むしめがね'
 
-    page_title.empty? ? base_title : page_title + ' | ' + base_title
+    page_title.empty? ? base_title : "#{page_title} | #{base_title}"
   end
 end
