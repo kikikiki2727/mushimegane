2f61eb41b85d734fdb31401d538f97ba16858401
kimori <kimori27271212@gmail.com>
2021-07-31 13:14:05 +0900

    Fix: pumaの設定を変更

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,6 +7,8 @@ set :repo_url, 'git@github.com:kikikiki2727/mushimegane.git'
 # Nginxの設定ファイル名と置き場所を修正
 set :nginx_config_name, "#{fetch(:application)}.conf"
 set :nginx_sites_enabled_path, '/etc/nginx/conf.d'
+set :rbenv_ruby, File.read('.ruby-version').strip
+set :branch, ENV['BRANCH'] || "master"
 
 append :linked_files, 'config/master.key'
 append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'node_modules'
