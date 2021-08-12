2dfe165f2712529e5ccb34e5a026484aa8700f8c
kikikiki2727 <kimori27271212@gmail.com>
2021-08-12 22:08:51 +0900

    Fix: dependent: :destroyを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   authenticates_with_sorcery!
 
-  has_many :bugs
+  has_many :bugs, dependent: :destroy
   # has_many :likes, dependent: :destroy
   # has_many :liked_comments, through: :likes, source: :comment
 
