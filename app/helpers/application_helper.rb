5b02b271d4dfee294944bd34b8a7fcdc40171ba3
kikikiki2727 <kimori27271212@gmail.com>
2021-09-14 14:24:29 +0900

    Fix: rubocop

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -26,23 +26,23 @@ module ApplicationHelper
       description: '家の中にどんな虫がいるのか検索してみよう！',
       keywords: '',
       canonical: 'https://mushimegane.fun/',
-      noindex: ! Rails.env.production?,
+      noindex: !Rails.env.production?,
       icon: [
         { href: image_url('mushimegane_top.jpeg') },
-        { href: image_url('mushimegane_top.jpeg'), rel: 'apple-touch-icon', sizes: '180x180', type: 'image/png' },
+        { href: image_url('mushimegane_top.jpeg'), rel: 'apple-touch-icon', sizes: '180x180', type: 'image/png' }
       ],
       og: {
         site_name: :site,
         title: :title,
-        description: :description, 
+        description: :description,
         type: 'website',
         url: :canonical,
         image: image_url('mushimegane_top.jpeg'),
-        locale: 'ja_JP',
+        locale: 'ja_JP'
       },
       twitter: {
         card: 'summary_large_image',
-        site: '@UDZVsLKlP1jnUTk',
+        site: '@UDZVsLKlP1jnUTk'
       }
     }
   end
